aaf962a175862352096e8b97b0537e4bc38e9cb8
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-03-19 19:38:17 +0200

    Add role seeding to the application startup

--- ErpProject/Program.cs
+++ ErpProject/Program.cs
@@ -1,18 +1,33 @@
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using ErpProject.ContextDb;
+using ErpProject.Models.EmployeeProfile;
+using System.Threading.Tasks;
 
 namespace MainProgram;
 
 class MainProgram
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
         builder.Services.AddDbContext<ErpDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
         var app = builder.Build();
+
+        using(var scope = app.Services.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<ErpDbContext>();
+            var adminRole = new Roles { RoleName = "Admin" };
+            var managerRole = new Roles { RoleName = "Manager" };
+            var userRole = new Roles { RoleName = "User" };
+
+            context.Roles.Add(adminRole);
+            context.Roles.Add(managerRole);
+            context.Roles.Add(userRole);
+            context.SaveChanges();
+        }
 
         app.Run();
     }
