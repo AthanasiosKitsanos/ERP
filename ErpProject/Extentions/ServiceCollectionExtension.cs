f34ee286295a37518ce5c133d19ac5dce6b9641a
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-03-22 17:01:29 +0200

    Add ServiceCollectionExtension for custom service registration

---
+++ ErpProject/Extentions/ServiceCollectionExtension.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ErpProject.Extentions;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddCustomServices(this IServiceCollection services)
+    {
+
+        return services;
+    }
+}
