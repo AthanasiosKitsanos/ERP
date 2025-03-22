63a355d27727db4caf1aa1df17adb3dbc6f38132
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-03-22 17:01:55 +0200

    Add AgeCalculator helper for calculating age from date of birth

---
+++ ErpProject/Helpers/AgeCalculator.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ErpProject.Helpers;
+
+public class AgeCalculator
+{
+    public static string CalculateAge(DateTime dateOfBirth)
+    {
+        var age = DateTime.Today.Year - dateOfBirth.Year;
+
+        if(dateOfBirth.Date > DateTime.Today.AddYears(-age))
+        {
+            age--;
+        }
+        
+        return age.ToString();
+    }
+}
