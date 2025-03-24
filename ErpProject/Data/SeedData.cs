10cc7e63a7af5252a43e1318c2a2c38e99aa8e7d
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-03-24 20:02:00 +0200

    Remove obj directory from tracking and update .gitignore

--- ErpProject/Data/SeedData.cs
+++ ErpProject/Data/SeedData.cs
@@ -36,71 +36,20 @@ public class SeedData
 
         if(employee is not null)
         {
-            // Adding more Additional Details
-            var settingsAdditionalDetails = FirstUserAdditinalDetails.GetJsonInfo();
+            var password = await _context.EmployeeCredentials.Join(_context.Employees, ec => ec.EmployeeId, e => e.Id, (ec, e) => new { ec, e })
+                .Where(emp => emp.e.Id == employee.Id)
+                .Select(e => e.ec.Password)
+                .FirstOrDefaultAsync();
 
-            var addDetailsExist = await _context.AdditionalDetails.AnyAsync(a => a.EmployeeId == employee.Id);
-
-            if(!addDetailsExist)
+            if(password is not null)
             {
-                var additionalDetails = new AdditionalDetails
+                var hashedPassword = Hashing.HashPassword(password);
+
+                if(hashedPassword is not null)
                 {
-                    EmergencyNumbers = settingsAdditionalDetails.EmergencyNumbers,
-                    Education = settingsAdditionalDetails.Education,
-                    Certifications = Encoding.ASCII.GetBytes("My Certification"),
-                    PersonalDocuments = Encoding.ASCII.GetBytes("My Personal Documents"),
-                    EmployeeId = employee.Id
-                };
-
-                await _context.AdditionalDetails.AddAsync(additionalDetails);
-                await _context.SaveChangesAsync();
-            }
-
-            // TODO: Add the user to the Identity methods to add to the database
-            // and create a method that checks if the TIN is a valid number
-
-            var settingsIdentification = FirstUserIdentification.GetJsonInfo();
-
-            var identification = new Identifications
-            {
-                TIN = settingsIdentification.TIN,
-                WorkAuth = settingsIdentification.WorkAuth,
-                TaxInformation = settingsIdentification.TaxInformation,
-                EmployeeId = employee.Id
-            };
-
-            var isValidTin = TinValidation.IsValidTin(identification.TIN);
-
-            if(isValidTin)
-            {
-                if(!_context.Identifications.Any(i => i.TIN == identification.TIN))
-                {
-                    await _context.Identifications.AddAsync(identification);
+                    await _context.EmployeeCredentials.Where(ec => ec.EmployeeId == employee.Id).ExecuteUpdateAsync(ec => ec.SetProperty(e => e.Password, hashedPassword));
                     await _context.SaveChangesAsync();
                 }
-            }
-
-            var accountStatus = await _context.AccountStatus.AnyAsync();
-
-            if(!accountStatus)
-            {
-                await _createElements.CreateAccountStatusAsync();
-            }
-
-            var settingsCredentials = FirstUserCredentials.GetJsonInfo();
-
-            var credentials = new EmployeeCredentials
-            {
-                Username = settingsCredentials.Username,
-                Password = settingsCredentials.Password,
-                EmployeeId = employee.Id,
-                AccountStatusId = await _context.AccountStatus.Where(s => s.StatusName == "Active").Select(s => s.Id).FirstOrDefaultAsync()
-            };
-
-            if(!_context.EmployeeCredentials.Any(e => e.Username == credentials.Username))
-            {
-                await _context.EmployeeCredentials.AddAsync(credentials);
-                await _context.SaveChangesAsync();
             }
         }
     }
