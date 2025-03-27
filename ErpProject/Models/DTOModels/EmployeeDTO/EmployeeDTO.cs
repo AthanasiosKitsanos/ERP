a125449feff5165ece2df0239f3ac057097d9103
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-03-27 20:39:51 +0200

    Add EmployeeDTO class for employee data transfer

---
+++ ErpProject/Models/DTOModels/EmployeeDTO/EmployeeDTO.cs
@@ -0,0 +1,26 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ErpProject.Models.DTOModels.EmployeeDTO;
+
+public class EmployeeDTO
+{
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    public string Age { get; set; } = string.Empty;
+
+    public DateTime DateOfBirth { get; set; }
+
+    public string Nationality { get; set; } = string.Empty;
+
+    public string Gender { get; set; } = string.Empty;
+
+    public string PhoneNumber { get; set; } = string.Empty;
+
+    public string PhotographPath { get; set; } = string.Empty;
+}
