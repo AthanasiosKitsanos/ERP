46d25002c24c2db9a4a9e6592e688e91976a9629
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-03-27 20:39:34 +0200

    Register EmployeeService and update service collection initialization

--- ErpProject/Extentions/ServiceCollectionExtension.cs
+++ ErpProject/Extentions/ServiceCollectionExtension.cs
@@ -1,6 +1,8 @@
 using System;
 using ErpProject.Data;
 using ErpProject.Helpers.InitializeFolder;
+using ErpProject.Interfaces;
+using ErpProject.Services.EmployeeServices;
 
 namespace ErpProject.Extentions;
 
@@ -8,8 +10,9 @@ public static class ServiceCollectionExtension
 {
     public static IServiceCollection AddCustomServices(this IServiceCollection services)
     {
-        services.AddScoped<CreateFirstElements>();
-        services.AddScoped<SeedData>();
+        services.AddScoped<IEmployeeService, EmployeeService>();
+        // services.AddScoped<CreateFirstElements>();
+        // services.AddScoped<SeedData>();
         return services;
     }
 }
