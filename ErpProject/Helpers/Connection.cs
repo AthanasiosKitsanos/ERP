2f7f64a741acfdedda0db3c45d9d7be6b1ca88e8
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-04-06 21:11:18 +0300

    Moved the Connection class to the Helpers folder

---
+++ ErpProject/Helpers/Connection.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ErpProject.Helpers.Connection;
+
+public class Connection
+{
+    public string ConnectionString { get ; set;}
+
+    public Connection(IConfiguration configuration)
+    {
+        ConnectionString = configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
+    }
+}
