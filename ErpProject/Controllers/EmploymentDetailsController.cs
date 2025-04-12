2d6f238f54e057118f8492687c3998240b0c02ee
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-04-12 20:57:41 +0300

    Add EmploymentDetailsController with add and addDetails actions for managing employment details

---
+++ ErpProject/Controllers/EmploymentDetailsController.cs
@@ -0,0 +1,34 @@
+using System;
+using ErpProject.Models.DTOModels;
+using ErpProject.Services.EmployeeServicesFolder;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ErpProject.Controllers;
+
+[Route("employmentDetails")]
+public class EmploymentDetailsController: Controller
+{
+
+    [HttpGet("add")]
+    public IActionResult Add(ViewModelDTO model)
+    {
+        if(model is null)
+        {
+            return NotFound("No information was found");
+        }
+
+        return View(model);
+    }
+
+    [HttpPost("add")]
+    [ValidateAntiForgeryToken]
+    public IActionResult AddDetails(ViewModelDTO model)
+    {
+        if(model is null)
+        {
+            return RedirectToAction("Add");
+        }
+
+        return RedirectToAction("Add", "Identifications", model);
+    }
+}
