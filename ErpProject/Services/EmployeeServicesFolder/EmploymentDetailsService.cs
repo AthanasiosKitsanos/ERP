c97294fd139bd3f31cb7fa0c87eb2b33ab4379db
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-04-12 20:58:07 +0300

    Add EmploymentDetailsService as a placeholder for managing employment details

---
+++ ErpProject/Services/EmployeeServicesFolder/EmploymentDetailsService.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace ErpProject.Services.EmployeeServicesFolder;
+
+public class EmploymentDetailsService
+{
+    //TODO: Finish the service and add it to the EmployeeService inside the complete registration
+}
