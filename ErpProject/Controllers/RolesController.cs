9a02168f7b70f1d29ab5c726e636dafe2a85151b
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-04-19 19:25:24 +0300

    Implement RolesController with methods for adding roles and retrieving role list

--- ErpProject/Controllers/RolesController.cs
+++ ErpProject/Controllers/RolesController.cs
@@ -1,10 +1,46 @@
+using ErpProject.Models.DTOModels;
+using ErpProject.Services.RoleServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ErpProject.Controllers;
 
 [Route("roles")]
-public class RolesController : Controller
+public class RolesController: Controller 
 {
-    
-}
+    private readonly RoleService _roleService;
 
+    public RolesController(RoleService roleService)
+    {
+        _roleService = roleService;
+    }
+
+    [HttpGet("add")]
+    public async Task<IActionResult> Add(ViewModelDTO model)
+    {
+        if(model is null)
+        {
+            return NotFound();
+        }
+
+        model.Roles.RolesNameList = await _roleService.GetAllRolesAsync();
+
+        if(model.Roles.RolesNameList.Count == 0)
+        {
+            return BadRequest("The was no list retrieved frome the database.");
+        }
+
+        return View(model);
+    }
+
+    [HttpPost("add")]
+    [ValidateAntiForgeryToken]
+    public IActionResult AddRoles(ViewModelDTO model)
+    {
+        if(model is null)
+        {
+            return NotFound();
+        }
+
+        return RedirectToAction("Add", "EmployeeCredentials", model);
+    }
+}
