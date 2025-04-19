22190455fe660aeb6c52da12363be831e517caec
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-04-19 19:25:34 +0300

    Add RoleService dependency to RegistrationModel and clean up using directives

--- ErpProject/Models/RegistrationModel.cs
+++ ErpProject/Models/RegistrationModel.cs
@@ -1,7 +1,10 @@
 using System;
 using ErpProject.Helpers.Connection;
 using ErpProject.Services.EmployeeServices;
-using ErpProject.Services.EmployeeServicesFolder;
+using ErpProject.Services.AdditionalDetailsServices;
+using ErpProject.Services.EmploymentDetailsServices;
+using ErpProject.Services.IdentificationServices;
+using ErpProject.Services.RoleServices;
 
 namespace ErpProject.Models.RegistrationModel;
 
@@ -15,11 +18,14 @@ public class RegistrationModel
 
     public IdentificationsService identifivationService;
 
-    public RegistrationModel(EmployeeService _employeeService, AdditionalDetailsService _additionalDetailsService, EmploymentDetailsService _employmentDetailsService, IdentificationsService _identifivationService)
+    public RoleService roleService;
+
+    public RegistrationModel(EmployeeService _employeeService, AdditionalDetailsService _additionalDetailsService, EmploymentDetailsService _employmentDetailsService, IdentificationsService _identifivationService, RoleService _roleService)
     {
         employeeService = _employeeService;
         additionalDetailsService = _additionalDetailsService;
         employmentDetailsService = _employmentDetailsService;
         identifivationService = _identifivationService;
+        roleService = _roleService;
     }
 }
