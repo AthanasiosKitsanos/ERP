b290622da3c19f4149fdb752f7c3b4dae567e4d5
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-04-19 19:28:25 +0300

    Implement constructor for EmployeeCredentialsService to initialize connection

--- ErpProject/Services/EmployeeServicesFolder/EmployeeCredentialsService.cs
+++ ErpProject/Services/EmployeeServicesFolder/EmployeeCredentialsService.cs
@@ -1,8 +1,16 @@
 using System;
+using ErpProject.Helpers.Connection;
 
 namespace ErpProject.Services.EmployeeCredentialsServices;
 
 public class EmployeeCredentialsService
 {
+    private readonly Connection _connection;
 
+    public EmployeeCredentialsService(Connection connection)
+    {
+        _connection = connection;
+    }
+
+    // TODO: Add methods to relate the credantials to the employee
 }
