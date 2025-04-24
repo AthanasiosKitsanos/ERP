c137f75cd815b4096ce253ae09f2026abc315cde
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-04-24 21:44:38 +0300

    Refactor AccountStatusDTO to include Id, SelectedStatus, and StatusList properties

--- ErpProject/Models/DTOModels/AccountStatusDTO.cs
+++ ErpProject/Models/DTOModels/AccountStatusDTO.cs
@@ -1,6 +1,11 @@
+using ErpProject.Models.AccountStatusModel;
+
 namespace ErpProject.Models.DTOModels;
 
 public class AccountStatusDTO
 {
-    public string StatusName { get; set; } = string.Empty;
+    public int Id { get; set; }
+
+    public int SelectedStatus { get; set; }
+    public List<AccountStatus> StatusList { get; set; } = new List<AccountStatus>();
 }
