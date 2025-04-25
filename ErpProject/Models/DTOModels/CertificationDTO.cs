e4601e893939e6e81321819bc3b118e65384121c
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-04-26 01:42:04 +0300

    Refactor: Remove unused DTOs and related services
    - Deleted IdentificationsDTO, PersonalDocumentsDTO, RolesDTO, and ViewModelDTO classes.
    - Removed AccountStatus, AdditionalDetails, Certifications, Employee, EmployeeCredentials, EmploymentDetails, Identifications, PersonalDocuments, RoleEmployee, and Roles models.
    - Eliminated corresponding services: AdditionalDetailsService, EmployeeCredentialsService, EmployeeService, EmploymentDetailsService, IdentificationsService, RoleService, and RegistrationService.
    - Removed related views for AdditionalDetails, EmployeeCredentials, EmploymentDetails, Identifications, Roles, and error handling.

--- ErpProject/Models/DTOModels/CertificationDTO.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace ErpProject.Models.DTOModels;
-
-public class CertificationDTO
-{
-    public List<string> CertificationPaths { get; set; } = new List<string>();
-    public int EmployeeId { get; set; }
-}
--- ErpProject/Models/DTOModels/RolesDTO.cs
+++
@@ -1,9 +0,0 @@
-using ErpProject.Models.RolesModel;
-
-namespace ErpProject.Models.DTOModels;
-
-public class RolesDTO
-{
-    public int SelectedRole { get; set; }
-    public List<Roles> RolesNameList { get; set; } = new List<Roles>();
-}
