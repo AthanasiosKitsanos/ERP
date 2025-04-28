39c3c6ec589e7e6f9f54e26036e5c70117567ed0
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-04-28 21:30:00 +0300

    Add RegistrationCleanUpService for periodic employee record cleanup

---
+++ ErpProject/BackgroundServices/RegistrationCleanUpService.cs
@@ -0,0 +1,34 @@
+using ErpProject.Helpers.Connection;
+using Microsoft.Data.SqlClient;
+
+namespace ErpProject.BackgroundServices;
+
+public class RegistrationCleanUpService: BackgroundService
+{
+    private readonly string _connectionString;
+
+    public RegistrationCleanUpService(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("DefaultConnection") ?? string.Empty;;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        string query = @"DELETE TOP(100) FROM Employees WHERE IsCompleted = 0";
+
+        while(!stoppingToken.IsCancellationRequested)
+        {
+            using(SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync(stoppingToken);
+
+                using(SqlCommand command = new SqlCommand(query, connection))
+                {
+                    await command.ExecuteNonQueryAsync(stoppingToken);
+                }
+            }
+
+            await Task.Delay(TimeSpan.FromMinutes(10), stoppingToken);   
+        }
+    }
+}
