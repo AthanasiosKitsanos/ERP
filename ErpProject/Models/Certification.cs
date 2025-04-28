1d77feaf0ece74bda1286ea22aa6b37d70e8768f
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-04-28 21:30:07 +0300

    Remove Certification and EmployeeCredentials models to streamline employee management

--- ErpProject/Models/Certification.cs
+++
@@ -1,9 +0,0 @@
-namespace ErpProject.Models;
-
-public class Certification
-{
-    public List<string> CertificationPaths { get; set; } = new List<string>();
-    public int EmployeeId { get; set; }
-
-    public List<string> FullPathList { get; set; } = new List<string>();
-}
