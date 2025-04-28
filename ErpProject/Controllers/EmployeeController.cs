c861a33554400f13750dd7aaff83229fec796a5d
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-04-28 21:30:33 +0300

    Refactor EmployeeController: Enhance registration and deletion logic with improved error handling and validation

--- ErpProject/Controllers/EmployeeController.cs
+++ ErpProject/Controllers/EmployeeController.cs
@@ -30,8 +30,69 @@ public class EmployeeController: Controller
     [HttpGet("register")]
     public IActionResult Register()
     {
-        Employee employee = new Employee();
+        return View();
+    }
+
+    [HttpPost("register")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Register(Employee employee)
+    {
+        if(!ModelState.IsValid)
+        {
+            return View(employee);
+        }
+
+        bool emailExists = await _service.EmailExistsAsync(employee.Email);
+
+        if(emailExists)
+        {
+            ModelState.AddModelError("Email", "Email already exists. Please try another.");
+            return View(employee);
+        }
+
+        int id = await _service.AddEmployeeAsync(employee);
+
+        if(id <= 0)
+        {
+            ModelState.AddModelError(string.Empty, "An unexpected error has occurred while saving. Please try again.");
+            return View(employee);
+        }
+
+        return RedirectToAction("Index", "Credentials", id);
+    }
+
+    [HttpGet("delete/{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        if(id <= 0)
+        {
+            return NotFound("No Id was found");
+        }
+
+        Employee employee = await _service.GetEmployeeByIdAsync(id);
+
+        employee.Id = id;
+
+        if(employee is null)
+        {
+            ModelState.AddModelError(string.Empty, "No employee was found.");
+            
+            return View("Index");
+        }
 
         return View(employee);
+    }
+
+    [HttpPost("delete/{id}")]
+    public async Task<IActionResult> ConfirmDelete(int id)
+    {
+        bool result =  await _service.DeleteEmployeeByIdAsync(id);
+
+        if(!result)
+        {
+            ModelState.AddModelError(string.Empty, "There was a problem with the deletion of the employee");
+        }
+
+        return RedirectToAction("Index");
     }
 }
