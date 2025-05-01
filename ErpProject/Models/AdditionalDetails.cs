9bd8301e9bce357b0467fa39dac313b18fbb3f2e
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-05-01 16:19:44 +0300

    Refactor AdditionalDetails model to remove Identifications and PersonalDocuments properties

--- ErpProject/Models/AdditionalDetails.cs
+++ ErpProject/Models/AdditionalDetails.cs
@@ -6,13 +6,9 @@ public class AdditionalDetails
 
     public string Education { get; set; } = string.Empty;
 
-    public byte[] Identifications { get; set; } = new byte[0];
-     public IFormFile? IdentificationFile { get; set; }
+    public IFormFile? CertificationFile { get; set; }
 
-    public byte[] PersonalDocuments { get; set; } = new byte[0];
     public IFormFile? PersonalDocumentsFile { get; set; }
-
-    public string MIME { get; set; } = string.Empty;
 
     public int EmployeeId { get; set; }
 }
