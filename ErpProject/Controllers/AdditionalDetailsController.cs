88590b407708ba1f8a19dfb20a978180fabfd36e
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-05-11 23:37:04 +0300

    Refactor AdditionalDetailsController to streamline action methods and improve error handling

--- ErpProject/Controllers/AdditionalDetailsController.cs
+++ ErpProject/Controllers/AdditionalDetailsController.cs
@@ -14,59 +14,95 @@ public class AdditionalDetailsController: Controller
         _service = service;
     }
 
-    [HttpGet("add/{id}")]  
-    public IActionResult Add(int id)
+    [HttpGet("index/{id}")]
+    public async Task<IActionResult> Index(int id)
+    {
+        if(id <= 0)
+        {
+            ModelState.AddModelError(string.Empty, "There was a problem loading the page");
+            return View();
+        }
+
+        AdditionalDetails details = await _service.GetAdditionalDetailsAsync(id);
+
+        if(details is null)
+        {
+            ModelState.AddModelError(string.Empty, "The are no details found");
+        }
+        
+        return PartialView(details);
+    }
+
+    [HttpGet("register/{id}")]  
+    public IActionResult Register(int id)
     {
         if(id <= 0)
         {
             ModelState.AddModelError(string.Empty, "The was no employee found");
-            return RedirectToAction("Index", "Employee");
+            return RedirectToAction("Details", "Employee");
         }
 
-        ViewData["Id"] = id;
+        AdditionalDetails details = new AdditionalDetails()
+        {
+            EmployeeId = id
+        };
 
-        return View();
+        return PartialView(details);
     }
 
-    [HttpPost("add/{id}")]
+    [HttpPost("register/{id}")]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Add(int id, AdditionalDetails details)
+    public async Task<IActionResult> Register(AdditionalDetails details)
     {
-        if(id == 0)
+        if(!ModelState.IsValid)
+        {
+            return PartialView(details);
+        }
+
+        if(details.EmployeeId <= 0)
         {
             return NotFound("Employee not Found");
         }
 
-        bool result = await _service.AddAdditionalDetailsAsync(id, details);
+        bool result = await _service.AddAdditionalDetailsAsync(details.EmployeeId, details);
 
         if(!result)
         {
             ModelState.AddModelError(string.Empty, "There was a problem while adding the new details or files.");
-            return RedirectToAction("Details", "Employee", new {id});
+            return RedirectToAction("Details", "Employee", new {details.EmployeeId});
         }
 
-        return RedirectToAction("Details", "Employee", new {id});
+        return RedirectToAction("Details", "Employee", new {details.EmployeeId});
     }
 
-    [HttpGet("update/{id}")]
-    public IActionResult Update(int id)
+    [HttpGet("edit/{id}")]
+    public IActionResult Edit(int id)
     {
-        ViewData["Id"] = id;
+        if(id <= 0)
+        {
+            ModelState.AddModelError(string.Empty, "The was no employee found");
+            return RedirectToAction("Details", "Employee");
+        }
+        
+        AdditionalDetails details = new AdditionalDetails()
+        {
+            EmployeeId = id
+        };
 
-        return View();
+        return PartialView(details);
     }
 
-    [HttpPost("update/{id}")]
+    [HttpPost("edit/{id}")]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Update(int id, AdditionalDetails details)
+    public async Task<IActionResult> Edit(AdditionalDetails details)
     {
-        bool result = await _service.UpdateAdditionalDetailsAsync(id, details);
+        bool result = await _service.UpdateAdditionalDetailsAsync(details.EmployeeId, details);
 
         if(!result)
         {
-            return RedirectToAction("Detials", "Employee", new{id});
+            return RedirectToAction("Detials", "Employee", new{details.EmployeeId});
         }
 
-        return RedirectToAction("Details", "Employee", new {id});
+        return RedirectToAction("Details", "Employee", new {details.EmployeeId});
     }
 }
