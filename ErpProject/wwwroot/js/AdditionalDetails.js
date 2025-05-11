3ae11a3befc51f68844c6155d3254b5d43281e39
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-05-11 23:36:42 +0300

    Add AdditionalDetails.js for handling form interactions and data fetching

---
+++ ErpProject/wwwroot/js/AdditionalDetails.js
@@ -0,0 +1,82 @@
+document.addEventListener("DOMContentLoaded", async function()
+{
+    const Id = window.Id;
+
+    let response = await fetch(`/AdditionalDetails/Index/${Id}`);
+
+    let html = await response.text();
+
+    let container = document.getElementById("additionalDetailsContainer");
+
+    container.innerHTML = html;
+
+    RegisterFormButton(Id);
+    
+    EditFormButton(Id);
+
+    function RegisterFormButton(Id)
+    {
+        const addForm = document.getElementById("AdRegistrationForm");
+
+        if(addForm)
+        {
+            addForm.addEventListener("click", async function(event)
+            {
+                event.preventDefault();
+
+                response = await fetch(`/AdditionalDetails/Register/${Id}`);
+
+                html = await response.text();
+
+                container.innerHTML = html;
+
+                CancelFormButton(Id);
+            });
+        }
+    }
+
+    function EditFormButton(Id)
+    {
+        const editForm = document.getElementById("AdEditForm");
+
+        if(editForm)
+        {
+            editForm.addEventListener("click", async function(event)
+            {
+                event.preventDefault();
+                
+                response = await fetch(`/AdditionalDetails/Edit/${Id}`);
+
+                html = await response.text();
+
+                container.innerHTML = html;
+
+                CancelFormButton(Id);
+            });
+        }
+    }
+
+    function CancelFormButton(Id)
+    {
+        const cancelForm = document.getElementById("CancelAdForm");
+
+        if(cancelForm)
+        {
+            cancelForm.addEventListener("click", async function(event)
+            {
+                event.preventDefault();
+
+                response = await fetch(`/AdditionalDetails/Index/${Id}`);
+
+                html = await response.text();
+
+                container.innerHTML = html;
+
+                RegisterFormButton(Id);
+                EditFormButton(Id);
+            });
+        }
+    }
+
+
+});
