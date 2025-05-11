dc4f7dc74710d7cd3450cf863e2ad7c79bed3b2e
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-05-11 23:36:47 +0300

    Add Employees.js for fetching and displaying employee data in a table

---
+++ ErpProject/wwwroot/js/Employees.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", async function()
+{
+    const tbody = document.getElementById('employeeTable');
+
+        fetch('/Employee/GetAllEmployees')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(e => {
+                const row = `<tr>
+                                <td>${e.firstName}</td>
+                                <td>${e.lastName}</td>
+                                <td>${e.email}</td>
+                                <td>${e.age}</td>
+                                <td>${e.dateOfBirth}</td>
+                                <td>${e.nationality}</td>
+                                <td>${e.gender}</td>
+                                <td>${e.phoneNumber}</td>
+                                <td><a href="/Employee/Details/${e.id}">Details</a></td>
+                                <td><a href="/Employee/Update/${e.id}">Edit</a></td>
+                                <td><a href="/Employee/Delete/${e.id}">Delete</a></td>
+                            </tr>`;
+                tbody.innerHTML += row;
+            });
+        })
+        .catch(error => console.error('Fetch error:', error));
+});
