80c824c1cedaa2cb24628f3de2d6264008a6544a
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-05-12 22:56:55 +0300

    Add route and HTTP GET attribute to HomeController for Index action

--- ErpProject/Controllers/HomeController.cs
+++ ErpProject/Controllers/HomeController.cs
@@ -2,8 +2,10 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ErpProject.Controllers;
 
+[Route("home")]
 public class HomeController: Controller
 {
+    [HttpGet("index")]
     public IActionResult Index()
     {
         return View();
