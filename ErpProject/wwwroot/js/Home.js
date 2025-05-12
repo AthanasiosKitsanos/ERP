f03746f4e3c0b27eb6dc13119751f5c4af3e90c4
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-05-12 22:57:31 +0300

    Add loading animation to work section on page load

---
+++ ErpProject/wwwroot/js/Home.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function ()
+{
+    let work = document.getElementById("work");
+
+    const text = ['.', '..', '...'];
+
+    let index = 0;
+
+    dotMethod();
+
+    function dotMethod()
+    {
+        if(work)
+        {   
+            work.innerHTML = `Work in Progress${text[index]}`;
+
+            index = (index + 1) % text.length;
+
+            setTimeout(dotMethod, 500);
+        }
+    }
+    
+});
