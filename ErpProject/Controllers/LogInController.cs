d51ea4dadd3a9b4bc5d686ae1df44ac887034e49
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-05-12 22:56:59 +0300

    Refactor LogInController to include route and HTTP GET attribute for Index action

--- ErpProject/Controllers/LogInController.cs
+++ ErpProject/Controllers/LogInController.cs
@@ -1,11 +1,20 @@
+using ErpProject.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ErpProject.Controllers;
 
+[Route("login")]
 public class LogInController: Controller
 {
+    [HttpGet("index")]
     public IActionResult Index()
     {
         return View();
     }
+
+    // [HttpPost("index")]
+    // public async Task<IActionResult> Index(LogIn login)
+    // {
+
+    // } 
 }
