9880fb0337048735a9763736333f8721bf8258fe
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-05-16 21:28:54 +0300

    Simplify AddCustomInterfaces method by removing CacheEmployeeServices instantiation

--- ErpProject/Extentions/InterfaceCollectionExtention.cs
+++ ErpProject/Extentions/InterfaceCollectionExtention.cs
@@ -8,16 +8,7 @@ public static class InterfaceCollectionExtention
 {
     public static IServiceCollection AddCustomInterfaces(this IServiceCollection services)
     {
-        services.AddScoped<IEmployeeServices>(sp => 
-        {
-            var realService = sp.GetRequiredService<EmployeeServices>();
-
-            var cache  = sp.GetRequiredService<IMemoryCache>();
-
-            var logger = sp.GetRequiredService<ILogger<CacheEmployeeServices>>();
-
-            return new CacheEmployeeServices(realService, cache, logger);
-        });
+        services.AddScoped<IEmployeeServices, EmployeeServices>();
 
         return services;
     }
