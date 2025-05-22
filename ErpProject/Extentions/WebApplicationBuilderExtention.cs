b7216a04883a9cdabba3e75c993cce7ae1ed55f1
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-05-22 23:06:40 +0300

    Update JSON file references in AddJsonFiles method for consistency

--- ErpProject/Extentions/WebApplicationBuilderExtention.cs
+++ ErpProject/Extentions/WebApplicationBuilderExtention.cs
@@ -4,9 +4,9 @@ public static class WebApplicationBuilderExtention
 {
     public static WebApplicationBuilder AddJsonFiles(this WebApplicationBuilder builder)
     {
-        builder.Configuration.AddJsonFile("demoSecretKey.json", optional: false, reloadOnChange: true);
+        builder.Configuration.AddJsonFile("DemoKey.json", optional: false, reloadOnChange: true);
         
-        builder.Configuration.AddJsonFile("header.json", optional: false, reloadOnChange: true);
+        builder.Configuration.AddJsonFile("JWTHeader.json", optional: false, reloadOnChange: true);
 
         return builder;
     }
