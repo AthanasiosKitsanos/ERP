b32ee0c3f0a8deb64209cc582aa7186494a2130d
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-05-25 14:30:56 +0300

    Remove JWTHeader.json configuration from AddJsonFiles method to streamline JSON file loading

--- ErpProject/Extentions/WebApplicationBuilderExtention.cs
+++ ErpProject/Extentions/WebApplicationBuilderExtention.cs
@@ -5,8 +5,6 @@ public static class WebApplicationBuilderExtention
     public static WebApplicationBuilder AddJsonFiles(this WebApplicationBuilder builder)
     {
         builder.Configuration.AddJsonFile("DemoKey.json", optional: false, reloadOnChange: true);
-        
-        builder.Configuration.AddJsonFile("JWTHeader.json", optional: false, reloadOnChange: true);
 
         return builder;
     }
