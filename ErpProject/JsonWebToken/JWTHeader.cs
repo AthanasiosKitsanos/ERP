af95a0101c9481325f9b65c5af1486c714011bb5
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-05-25 14:31:05 +0300

    Refactor JWTHeader class to use default values for algorithm and type, removing unnecessary configuration dependency

--- ErpProject/JsonWebToken/JWTHeader.cs
+++ ErpProject/JsonWebToken/JWTHeader.cs
@@ -4,15 +4,8 @@ namespace ErpProject.JsonWebToken;
 
 public class JWTHeader
 {
-    public string algorithm { get; set; } = string.Empty;
-    public string type { get; set; } = string.Empty;
-
-    public JWTHeader(IConfiguration configuration)
-    {
-        IConfiguration section = configuration.GetSection("HeaderSetting");
-        algorithm = section.GetValue<string>("algorithm") ?? string.Empty;
-        type = section.GetValue<string>("type") ?? string.Empty;
-    }
+    public string algorithm { get; set; } = "HS256";
+    public string type { get; set; } = "JWT";
 
     public string Serialize() => JsonSerializer.Serialize(this);
 }
