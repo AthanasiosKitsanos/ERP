38ee80c3565ef71aece29436119fbd66c3d36b6f
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-05-25 14:31:08 +0300

    Refactor JWTPayload class to set default values for Issuer and Audience, and initialize TokenValidityOnSeconds

--- ErpProject/JsonWebToken/JWTPayload.cs
+++ ErpProject/JsonWebToken/JWTPayload.cs
@@ -5,26 +5,14 @@ namespace ErpProject.JsonWebToken;
 
 public class JWTPayload
 {
-    public string Issuer { get; set; } = string.Empty;
-    public string Audience { get; set; } = string.Empty;
+    public string Issuer { get; set; } = "http://localhost:5099";
+    public string Audience { get; set; } = "http://localhost:5099";
     public int Id { get; set; }
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public string Role { get; set; } = string.Empty;
-    public long TokenValidityOnSeconds { get; set; }
+    public long TokenValidityOnSeconds { get; set; } = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
     public long Expiration { get; set; }
-
-    public JWTPayload(LoggedInData data, bool rememberMe)
-    {
-        Issuer = "http://localhost:5099";
-        Audience = "http://localhost:5099";
-        Id = data.Id;
-        FirstName = data.FirstName;
-        LastName = data.LastName;
-        Role = data.RoleName;
-        TokenValidityOnSeconds = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-        Expiration = rememberMe ? DateTimeOffset.UtcNow.AddMonths(1).ToUnixTimeSeconds() : DateTimeOffset.UtcNow.AddHours(1).ToUnixTimeSeconds();
-    }
 
     public string Serialize() => JsonSerializer.Serialize(this);
 }
