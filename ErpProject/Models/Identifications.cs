f1954711f54761a3742344a01acb83c389d83639
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-05-30 00:39:23 +0300

    Refactor Identifications model to change WorkAuth type from bool to string and standardize Employee_Id to EmployeeId

--- ErpProject/Models/Identifications.cs
+++ ErpProject/Models/Identifications.cs
@@ -5,7 +5,7 @@ public class Identifications
 {
     public int Id;
     public string TIN { get; set; } = string.Empty;
-    public bool WorkAuth { get; set; }
+    public string WorkAuth { get; set; } = string.Empty;
     public string TaxInformation { get; set; } = string.Empty;
-    public int Employee_Id;
+    public int EmployeeId;
 }
