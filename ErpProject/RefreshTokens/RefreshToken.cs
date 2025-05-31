7fcac5f9d6f86955b1cd9d62ce8eac78927bfa58
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-05-31 16:23:06 +0300

    Add RefreshToken class to manage token expiration and revocation

---
+++ ErpProject/RefreshTokens/RefreshToken.cs
@@ -0,0 +1,9 @@
+namespace ErpProject.RefreshTokens;
+
+public class RefreshToken
+{
+    public Guid Id { get; set; }
+    public string Token { get; set; } = string.Empty;
+    public DateTime ExpiresAt { get; set; }
+    public DateTime? RevokedAt { get; set; }
+}
