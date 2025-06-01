5354a97d9fc8443d7e522796df2391fe13a5aff6
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-06-01 14:55:41 +0300

    Remove RefreshToken class as it is no longer needed

--- ErpProject/RefreshTokens/RefreshToken.cs
+++
@@ -1,9 +0,0 @@
-namespace ErpProject.RefreshTokens;
-
-public class RefreshToken
-{
-    public Guid Id { get; set; }
-    public string Token { get; set; } = string.Empty;
-    public DateTime ExpiresAt { get; set; }
-    public DateTime? RevokedAt { get; set; }
-}
