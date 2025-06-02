2a12954054c9e2dd9ee69d710d87b8b605349806
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-06-02 23:01:24 +0300

    Add RevokeRefreshTokenAsync method to RefreshTokenServices for token revocation functionality

--- ErpProject/RefreshTokens/RefreshTokenServices.cs
+++ ErpProject/RefreshTokens/RefreshTokenServices.cs
@@ -114,4 +114,31 @@ public class RefreshTokenServices
 
         return id;
     }
+
+    public async Task<bool> RevokeRefreshTokenAsync(string token, string ipAddress)
+    {
+        if (string.IsNullOrEmpty(token) || string.IsNullOrEmpty(ipAddress))
+        {
+            return false;
+        }
+
+        string query = @"UPDATE RefreshToken 
+                        SET RevokedAt = @RevokedAt,
+                        RevokedByIp = @RevokedByIp
+                        WHERE Token = @Token";
+
+        await using (SqlConnection connection = new SqlConnection(_connection.ConnectionString))
+        {
+            await connection.OpenAsync();
+
+            await using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.Add("@RevokedAt", SqlDbType.DateTime2).Value = DateTime.UtcNow;
+                command.Parameters.Add("@Token", SqlDbType.NVarChar).Value = token;
+                command.Parameters.Add("@RevokedByIp", SqlDbType.NVarChar).Value = ipAddress;
+
+                return await command.ExecuteNonQueryAsync() > 0;
+            }
+        }
+    }
 }
