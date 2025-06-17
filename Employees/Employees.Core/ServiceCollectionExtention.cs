20613a1d0d347db5aea6ca056a52d62d6d964219
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-06-17 18:02:36 +0300

    Add ServiceCollectionExtention to register core services

---
+++ Employees/Employees.Core/ServiceCollectionExtention.cs
@@ -0,0 +1,17 @@
+using Employees.Core.IServices;
+using Employees.Core.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Employees.Core;
+
+public static class ServiceCollectionExtention
+{
+    public static IServiceCollection AddCore(this IServiceCollection services)
+    {
+        services.AddScoped<IEmployeesServices, EmployeesServices>();
+
+        //services.AddScoped<IEmployeesValidators, EmployeesValidators>();
+        
+        return services;
+    }
+}
