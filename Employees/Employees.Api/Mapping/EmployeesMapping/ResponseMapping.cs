55f5cbd5f5e3e196e687e8df2d91547d858e9e4b
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-06-19 00:28:01 +0300

    Add ResponseMapping class for mapping employee response data

---
+++ Employees/Employees.Api/Mapping/EmployeesMapping/ResponseMapping.cs
@@ -0,0 +1,36 @@
+using System;
+using Employees.Contracts.Employee;
+using Employees.Domain.Models;
+
+namespace Employees.Api.Mapping.Employees;
+
+public static class ResponseMapping
+{
+    public static ResponseEmployee.Get MapToGetResponse(this Employee employee)
+    {
+        return new ResponseEmployee.Get
+        {
+            Id = employee.Id,
+            FirstName = employee.FirstName,
+            LastName = employee.LastName,
+            Email = employee.Email,
+            Age = employee.Age,
+            DateOfBirth = employee.DateOfBirth,
+            Nationality = employee.Nationality,
+            Gender = employee.Gender,
+            PhoneNumber = employee.PhoneNumber,
+            Photograph = employee.Photograph,
+            MIME = employee.MIME
+        };
+    }
+
+    public static ResponseEmployee.Delete MapToDeleteResponse(this Employee employee)
+    {
+        return new ResponseEmployee.Delete
+        {
+            Id = employee.Id,
+            FirstName = employee.FirstName,
+            LastName = employee.LastName
+        };
+    }
+}
