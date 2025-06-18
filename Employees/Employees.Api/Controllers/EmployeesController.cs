64a3c6b7b7239601055359f0735bc0634edfebdc
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-06-18 20:37:21 +0300

    Refactor EmployeesController to include logging and improve GetAll method response handling

--- Employees/Employees.Api/Controllers/EmployeesController.cs
+++ Employees/Employees.Api/Controllers/EmployeesController.cs
@@ -1,3 +1,5 @@
+using Employees.Api.Mapping.Employees;
+using Employees.Contracts;
 using Employees.Core.IServices;
 using Employees.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -7,14 +9,16 @@ namespace Employees.Api.Controllers;
 public class EmployeesController : Controller
 {
     private readonly IEmployeesServices _services;
+    private readonly ILogger<EmployeesController> _logger;
 
-    public EmployeesController(IEmployeesServices services)
+    public EmployeesController(IEmployeesServices services, ILogger<EmployeesController> logger)
     {
         _services = services;
+        _logger = logger;
     }
 
     [HttpGet(Endpoint.Employees.Index)]
-    public IActionResult Index(CancellationToken cancellationToken)
+    public IActionResult Index()
     {
         return View();
     }
@@ -22,13 +26,20 @@ public class EmployeesController : Controller
     [HttpGet(Endpoint.Employees.GetAllEmployees)]
     public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
     {
-        List<Employee> employees = new List<Employee>();
+        if (cancellationToken.IsCancellationRequested)
+        {
+            _logger.LogWarning("Get All Employees cancelled by client");
+            cancellationToken.ThrowIfCancellationRequested();
+        }
+
+        List<ResponseEmployee> responseList = new List<ResponseEmployee>();
 
         await foreach (Employee employee in _services.GetAllAsync(cancellationToken))
         {
-            employees.Add(employee);
+            ResponseEmployee response = employee.MapToResponse();
+            responseList.Add(response);
         }
 
-        return Json(employees);
+        return Json(responseList);
     }
 }
