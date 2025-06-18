21ea094734c24aa5fc39d36b91c94857fd27e002
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-06-18 16:55:23 +0300

    Remove unnecessary await for refreshCheckCompleted in Employees.js

--- Employees/Employees.Api/wwwroot/js/Employees.js
+++ Employees/Employees.Api/wwwroot/js/Employees.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", async function()
-{
-    await window.refreshCheckCompleted;
-    
+{   
     const tbody = document.getElementById('employeeTable');
 
         fetch('/employees/getallemployees')
