eaca4992286d2aa319fd816517afe6e03dcde27f
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-06-19 23:17:54 +0300

    Add AdditionalDetailsRepository class implementing IAdditionalDetailsRepository

---
+++ Employees/Employees.Infrastructure/Repository/AdditionalDetailsRepository.cs
@@ -0,0 +1,30 @@
+using Employees.Domain;
+using Employees.Domain.Models;
+using Employees.Infrastructure.IRepository;
+
+namespace Employees.Infrastructure.Repository;
+
+public class AdditionalDetailsRepository : IAdditionalDetailsRepository
+{
+    private readonly Connection _connection;
+
+    public AdditionalDetailsRepository(Connection connection)
+    {
+        _connection = connection;
+    }
+
+    public Task<bool> CreateAsync(AdditionalDetails details, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<AdditionalDetails> GetAsync(int id, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateAsync(int id, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+}
