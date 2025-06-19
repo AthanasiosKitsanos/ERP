511485646f9f8fa2684c8357cd39a204661a08ff
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-06-19 21:07:47 +0300

    Fix typo in GetInfoForDeleteAysnc method name in IEmployeesRepository interface

--- Employees/Employees.Infrastructure/IRepository/IEmployeesRepository.cs
+++ Employees/Employees.Infrastructure/IRepository/IEmployeesRepository.cs
@@ -5,7 +5,7 @@ namespace Employees.Infrastructure.IRepository;
 public interface IEmployeesRepository
 {
     Task<bool> EmailExistsAsync(string email, CancellationToken token = default);
-    
+
     Task<int> CreateAsync(Employee employee, CancellationToken token = default);
 
     IAsyncEnumerable<Employee> GetAllAsync(CancellationToken token = default);
@@ -15,4 +15,6 @@ public interface IEmployeesRepository
     Task<bool> UpdateAsync(int id, Employee employee, CancellationToken token = default);
 
     Task<bool> DeleteByIdAsync(int id, CancellationToken token = default);
+
+    Task<Employee> GetInfoForDeleteAysnc(int id, CancellationToken token = default);
 }
