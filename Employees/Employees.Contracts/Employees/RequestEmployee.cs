61cb48fbec1bfdbf7a78690c4d60e8c50b3d7865
AthanasiosKitsanos <kitsanos.dev@gmail.com>
2025-06-21 02:49:12 +0300

    Refactor RequestEmployee class properties to use init-only setters for immutability

--- Employees/Employees.Contracts/Employees/RequestEmployee.cs
+++ Employees/Employees.Contracts/Employees/RequestEmployee.cs
@@ -6,19 +6,37 @@ public class RequestEmployee
 {
     public class Create
     {
-        public string FirstName { get; set; } = string.Empty;
+        public string FirstName { get; init; } = string.Empty;
 
-        public string LastName { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
+        public string LastName { get; init; } = string.Empty;
+        public string Email { get; init; } = string.Empty;
 
-        public DateOnly DateOfBirth { get; set; }
+        public DateOnly DateOfBirth { get; init; }
 
-        public string Nationality { get; set; } = string.Empty;
+        public string Nationality { get; init; } = string.Empty;
 
-        public string Gender { get; set; } = string.Empty;
+        public string Gender { get; init; } = string.Empty;
 
-        public string PhoneNumber { get; set; } = string.Empty;
+        public string PhoneNumber { get; init; } = string.Empty;
 
-        public required IFormFile PhotoFile { get; set; }
+        public required IFormFile PhotoFile { get; init; }
+    }
+
+    public class Update
+    {
+        public int Id { get; init; }
+        public string FirstName { get; init; } = string.Empty;
+        public string LastName { get; init; } = string.Empty;
+        public string Email { get; init; } = string.Empty;
+        public string Age { get; init; } = string.Empty;
+        public DateOnly DateOfBirth { get; init; }
+        public string Nationality { get; init; } = string.Empty;
+        public string Gender { get; init; } = string.Empty;
+        public string PhoneNumber { get; init; } = string.Empty;
+    }
+
+    public class Photo
+    {
+        public required IFormFile PhotoFile { get; init; }
     }
 }
