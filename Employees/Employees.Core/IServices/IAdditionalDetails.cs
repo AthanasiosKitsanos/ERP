0d2c74459972a0bc4afa1ceef086627926639a3a
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2025-06-24 20:05:53 +0300

    Add IAdditionalDetails interface to Employees.Core.IServices namespace

---
+++ Employees/Employees.Core/IServices/IAdditionalDetails.cs
@@ -0,0 +1,10 @@
+using Employees.Contracts.
+
+namespace Employees.Core.IServices;
+
+public interface IAdditionalDetails
+{
+    Task<bool> CreateAsync(RequestAdditionalDetails.Create createDetails, CancellationToken token = default);
+    Task<bool> UpdateAsync(RequestAdditionalDetails.Update updateRequest, CancellationToken token = default);
+    Task<ResponseAdditionalDetails.Get> GetAsync(int id, CancellationToken token = default);
+}
