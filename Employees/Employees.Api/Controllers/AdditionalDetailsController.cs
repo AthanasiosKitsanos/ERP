ed6bcd10e18a4fb19f103e9d1874dfeb60649a43
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2025-06-24 20:33:36 +0300

    Add AdditionalDetailsController with dependency on AdditionalDetailsServices

---
+++ Employees/Employees.Api/Controllers/AdditionalDetailsController.cs
@@ -0,0 +1,9 @@
+using Employees.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employees.Api.Controllers;
+
+public class AdditionalDetailsController: Controller
+{
+    private readonly AdditionalDetailsServices _services;
+}
