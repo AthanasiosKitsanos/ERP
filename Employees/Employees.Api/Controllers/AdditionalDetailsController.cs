95230aa14f8180d7dcea5acc2438bde569368644
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2025-06-25 21:03:07 +0300

    Add logging and cancellation support to Get method in AdditionalDetailsController

--- Employees/Employees.Api/Controllers/AdditionalDetailsController.cs
+++ Employees/Employees.Api/Controllers/AdditionalDetailsController.cs
@@ -1,4 +1,6 @@
+using Employees.Contracts.AdditionalDetailsContract;
 using Employees.Core.IServices;
+using Employees.Domain;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Employees.Api.Controllers;
@@ -6,9 +8,40 @@ namespace Employees.Api.Controllers;
 public class AdditionalDetailsController : Controller
 {
     private readonly IAdditionalDetailsServices _services;
+    private readonly ILogger<AdditionalDetailsController> _logger;
 
-    public AdditionalDetailsController(IAdditionalDetailsServices services)
+    public AdditionalDetailsController(IAdditionalDetailsServices services, ILogger<AdditionalDetailsController> logger)
     {
         _services = services;
+        _logger = logger;
     }
+
+    [HttpGet(Endpoint.AdditionalDetails.Get)]
+    public async Task<IActionResult> Get(int id, CancellationToken token)
+    {
+        if (token.IsCancellationRequested)
+        {
+            _logger.LogWarning("Request cancelled by user");
+            token.ThrowIfCancellationRequested();
+        }
+
+        ResponseAdditionalDetails.Get details = await _services.GetAsync(id, token);
+
+        if (details is null)
+        {
+            _logger.LogWarning($"{nameof(details)} were not found");
+
+            return PartialView("Error", new ErrorViewModel
+            {
+                StatusCode = 404,
+                Message = "Additional Details not found"
+            });
+        }
+
+        _logger.LogInformation($"{nameof(details)} are sent to employees/{id}/details\nFrom Url employees/{id}/additionaldetails");
+
+        return PartialView(details);
+    }
+
+    
 }
