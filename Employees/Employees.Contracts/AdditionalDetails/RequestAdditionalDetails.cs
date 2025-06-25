ae4914187a41f6b6b0b1652b1b85bb4fed805b51
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2025-06-25 21:03:28 +0300

    Refactor RequestAdditionalDetails class to improve structure and consistency in Create and Update models

--- Employees/Employees.Contracts/AdditionalDetails/RequestAdditionalDetails.cs
+++ Employees/Employees.Contracts/AdditionalDetails/RequestAdditionalDetails.cs
@@ -1,18 +1,24 @@
-using System;
+using Microsoft.AspNetCore.Http;
 
-namespace Employees.Contracts.AdditionalDetails;
+namespace Employees.Contracts.AdditionalDetailsContract;
 
 public class RequestAdditionalDetails
 {
     public class Create
     {
+        public string EmergencyNumbers { get; set; } = string.Empty;
 
+        public string Education { get; set; } = string.Empty;
+
+        public IFormFile? CertificationFile { get; set; }
+
+        public IFormFile? PersonalDocumentsFile { get; set; }
     }
 
     public class Update
     {
+        public string EmergencyNumbers { get; set; } = string.Empty;
 
+        public string Education { get; set; } = string.Empty;
     }
-
-
 }
