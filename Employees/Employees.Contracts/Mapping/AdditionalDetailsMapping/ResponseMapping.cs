0adc172663a2d01da350c286a26d77a609ae6f0e
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2025-06-25 21:03:36 +0300

    Refactor ResponseMapping class to improve structure and enhance mapping functionality

--- Employees/Employees.Contracts/Mapping/AdditionalDetailsMapping/ResponseMapping.cs
+++ Employees/Employees.Contracts/Mapping/AdditionalDetailsMapping/ResponseMapping.cs
@@ -1,8 +1,16 @@
-using System;
+using Employees.Contracts.AdditionalDetailsContract;
+using Employees.Domain.Models;
 
 namespace Employees.Contracts.AdditionalDetailsMapping;
 
-public class ResponseMapping
+public static class ResponseMapping
 {
-    
+    public static ResponseAdditionalDetails.Get MapToGetResponse(this AdditionalDetails details)
+    {
+        return new ResponseAdditionalDetails.Get
+        {
+            EmergencyNumbers = details.EmergencyNumbers,
+            Education = details.Education
+        };
+    }    
 }
