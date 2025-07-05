aadce198f2153f214bb298da27060403afcf6927
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2025-07-05 21:37:14 +0300

    changed the cs files to be partial classes of the razor files

--- Employees/Employees.RazorComponents/EmployeeComponents/GetAllEmployees.razor.cs
+++ Employees/Employees.RazorComponents/EmployeeComponents/GetAllEmployees.razor.cs
@@ -1,16 +1,17 @@
-using Employees.Shared.CustomEndpoints;
-using System.Net.Http.Json;
 using Employees.Contracts.EmployeeContracts;
+using Microsoft.AspNetCore.Components;
+using MvcStyle.Services;
 
 namespace Employees.RazorComponents.EmployeeComponents;
 
-public class GetAllEmployeesComponent: BaseComponent
+public partial class GetAllEmployees
 {
+    [Inject] private IControllerServices Controller { get; set; } = default!;
     protected List<ResponseEmployee.Get> employeeList { get; set; } = new List<ResponseEmployee.Get>();
 
     protected override async Task OnInitializedAsync()
     {
-        employeeList = await HttpClient.GetFromJsonAsync<List<ResponseEmployee.Get>>($"{Navigate.BaseUri.TrimEnd('/')}{Endpoints.Employees.GetAllEmployees}") ?? null!;
+        employeeList = await Controller.HttpGetJsonAsync<List<ResponseEmployee.Get>>("GetAll", "Employees");
     }
     
 }
