1ca3f9dabcf6793881e598ffb48eb3c133eedc02
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2025-07-05 21:40:16 +0300

    its a class that inherits from ComponentBase and adding a ViewMode enum

--- Employees/Employees.RazorComponents/BaseComponent.cs
+++ Employees/Employees.RazorComponents/BaseComponent.cs
@@ -11,7 +11,5 @@ public enum ViewMode
 
 public class BaseComponent : ComponentBase
 {
-    [Inject] protected NavigationManager Navigate { get; set; } = default!;
-    [Inject] protected HttpClient HttpClient { get; set; } = default!;
     protected ViewMode Mode = ViewMode.View;
 }
