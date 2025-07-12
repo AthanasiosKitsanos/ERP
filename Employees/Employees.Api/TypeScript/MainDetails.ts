7150497bb8408001c63c8e5313bec23165605da9
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2025-07-12 14:14:15 +0300

    added view, update functions

---
+++ Employees/Employees.Api/TypeScript/MainDetails.ts
@@ -0,0 +1,140 @@
+import { Employee } from "./Models/Employee";
+
+function formatDate(date: Date): string
+{
+    const day = String(date.getDate()).padStart(2, "0"); 
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+}
+
+async function cancelForm(id: number, container: HTMLDivElement, employee: Employee): Promise<void>
+{
+    const cancel = document.getElementById("cancelDetailsForm");
+
+    if(!cancel)
+    {
+        return;
+    }
+
+    cancel.addEventListener("click", async e =>
+    {
+        e.preventDefault();
+        await getView(id, container, employee);
+    })
+} 
+
+async function getView(id: number, container: HTMLDivElement, employee: Employee):Promise<void>
+{
+    const response: Response = await fetch(`/employees/${id}/getmaindetails`);
+    const html: string = await response.text();
+    
+    container.innerHTML = html;
+
+    document.getElementById("FirstName")!.innerText = employee.firstName ?? "";
+    document.getElementById("LastName")!.innerText = employee.lastName ?? "";
+    document.getElementById("Email")!.innerText = employee.email ?? "";
+    document.getElementById("Age")!.innerText = employee.age ?? "";
+
+    if(employee.dateOfBirth)
+    {
+        document.getElementById("DateOfBirth")!.innerText = formatDate(new Date(employee.dateOfBirth));
+    }
+    
+    document.getElementById("Nationality")!.innerText = employee.nationality ?? "";
+    document.getElementById("Gender")!.innerText = employee.gender ?? "";
+    document.getElementById("PhoneNumber")!.innerText = employee.phoneNumber ?? "";
+
+    await getUpdateView(id, container, employee);
+}
+
+async function getUpdateView(id: number, container: HTMLDivElement, employee: Employee): Promise<void>
+{
+    const formButton =  document.getElementById("updateDetailsForm") as HTMLAnchorElement;
+
+    if(!formButton)
+    {
+        return;
+    }
+
+    formButton.addEventListener("click", async e =>
+    {
+        e.preventDefault();
+
+        const response: Response = await fetch(`/employees/${id}/update`);
+        const html: string = await response.text();
+        container.innerHTML = html;
+
+        document.getElementById("FirstName")!.innerText = employee.firstName ?? "";
+        document.getElementById("LastName")!.innerText = employee.lastName ?? "";
+        document.getElementById("Age")!.innerText = employee.age ?? "";
+        if(employee.dateOfBirth)
+        {
+            document.getElementById("DateOfBirth")!.innerText = formatDate(new Date(employee.dateOfBirth));
+        }
+        document.getElementById("Gender")!.innerText = employee.gender ?? "";
+
+        await submitDetails(id, container);
+
+        await cancelForm(id, container, employee);
+    })   
+}
+
+async function submitDetails(id: number, container: HTMLDivElement): Promise<void>
+{
+    const onSubmit = document.getElementById("employee-update") as HTMLFormElement;
+
+    if(!onSubmit)
+    {
+        alert("There was something wrong");
+        return;
+    }
+
+    onSubmit.addEventListener("submit", async e =>
+    {
+        e.preventDefault();
+
+        const form = e.target as HTMLFormElement;
+        const data = new FormData(form);
+
+        const response: Response = await fetch(`/${id}/getmaindetails/update`, {method: 'POST', body: data})
+
+        const result = await response.json();
+
+        if(!result.success)
+        {
+            alert("Employee details were not updated");
+            return;
+        }
+        
+        const employee: Employee = await getEmployee(id);
+        
+        await getView(id, container, employee);
+    })
+}
+
+async function getEmployee(id: number):Promise<Employee>
+{
+    const response = await fetch(`/${id}/getmaindetails`);
+
+    const employee: Employee = await response.json();
+
+    if(!employee)
+    {
+        alert("No employee was found");
+    }
+
+    return employee;
+}
+
+document.addEventListener("DOMContentLoaded", async () =>
+{
+    const id: number = window.Id;
+
+    const employee: Employee = await getEmployee(id);
+
+    let container = document.getElementById("mainDetails") as HTMLDivElement;
+    
+    await getView(id, container, employee);
+})
