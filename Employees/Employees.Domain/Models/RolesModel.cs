ee55de194775a3ed8678e9f7843ba4090c427504
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2025-07-17 19:18:38 +0300

    Deleted the RoleList

--- Employees/Employees.Domain/Models/RolesModel.cs
+++ Employees/Employees.Domain/Models/RolesModel.cs
@@ -1,13 +1,5 @@
 namespace Employees.Domain.Models;
 
-public class RolesList
-{
-    public int EmployeeId { get; set; }
-    public int SelectedRoleId { get; set; }
-
-    public List<Roles> List { get; set; } = new List<Roles>();
-}
-
 public class Roles
 {
     public int Id { get; set; }
